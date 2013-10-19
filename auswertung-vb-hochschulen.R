9434c6f68f87ab9cf4cc5f74ca66113a4201fe20
Florian Jenn <devel@effjot.net>
2013-10-19 11:19:45 +0200

    Vergleiche mit Ergebnis Volksbegehren Nachtflugverbot

--- auswertung-vb-hochschulen.R
+++ auswertung-vb-hochschulen.R
@@ -1,4 +1,4 @@
-#### Auswertung (Karten, Plots) für die Ergebnisse des Volksbegehrens "Hochschulen erhalten"
+#### Auswertung (Karten, Plots) fÃ¼r die Ergebnisse des Volksbegehrens "Hochschulen erhalten"
 
 
 ### Ergebnisse laden
@@ -9,11 +9,14 @@ eintr.zw <- read.csv("zwischenstand.csv", as.is = TRUE, fileEncoding = "latin1")
 gemeinden.zw <- read.csv("zwischenstand-gemeinden.csv", as.is = TRUE, fileEncoding = "latin1")
 coordinates(gemeinden.zw) <- ~ lon + lat   # Koordinatenspalten zuweisen
 
-## vorläufiges Endergebnis
+## vorlÃ¤ufiges Endergebnis
 eintr.ve <- read.csv("vorlf-endergebnis.csv", as.is = TRUE, fileEncoding = "latin1")
 
 ## beides in ein dataframe
 eintr.all <- merge(eintr.zw, eintr.ve, by = "Landkreis", suffixes = c(".zw", ".ve"))
+
+## Nachtflugverbot
+nachtflug <- read.csv("nachtflugverbot.csv", as.is = TRUE, fileEncoding = "latin1")
 
 
 ### Landkreise in Deutschland von http://gadm.org/
@@ -24,12 +27,12 @@ load("DEU_adm3.RData")
 brandenburg <- gadm[gadm$NAME_1 == "Brandenburg", ]
 colnames(brandenburg@data)[colnames(brandenburg@data) == "NAME_3"] <- "Landkreis"
 
-## Namen bereinigen (kreisfr. Städte; Frankfurt)
-brandenburg$Landkreis <- gsub(" Städte", "", brandenburg$Landkreis)
+## Namen bereinigen (kreisfr. StÃ¤dte; Frankfurt)
+brandenburg$Landkreis <- gsub(" StÃ¤dte", "", brandenburg$Landkreis)
 brandenburg$Landkreis <- gsub("am Oder", "(Oder)", brandenburg$Landkreis)
 
 
-### Auswertung (zusätzliche Kennzahlen)
+### Auswertung (zusÃ¤tzliche Kennzahlen)
 
 ## Prozentanteil Briefwahl an Gesamtstimmenzahl
 eintr.ve$Briefprozent <- round(eintr.ve$Brief/eintr.ve$Anzahl * 100)
@@ -39,6 +42,10 @@ eintr.all$zuwachs <- eintr.all$Anzahl.ve - eintr.all$Anzahl.zw
 eintr.all$zuwachsprozent.zw <- round(eintr.all$zuwachs/eintr.all$Anzahl.zw * 100)
 eintr.all$zuwachsprozent.ges <- round(eintr.all$zuwachs/eintr.all$Anzahl.ve * 100)
 
+## Unterschied Nachtflugverbot, Hochschulen erhalten
+
+eintr.all$d.nachtflug.prozpkt <- eintr.ve$Prozent - nachtflug$Prozent
+
 
 ### Karten zeichnen
 
@@ -46,7 +53,7 @@ vb.file.standard.maps("zwischenstand", brandenburg, eintr.zw, gemeinden.zw,
                       "Zwischenstand 10.07.13",
                        prozent.klassen = c(0, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10))
 vb.file.standard.maps("vorl-endergebnis", brandenburg, eintr.ve, NULL,
-                      "Vorläufiges Endergebnis 09.10.13")
+                      "VorlÃ¤ufiges Endergebnis 09.10.13")
 
 png("vorl-endergebnis-brief.png", width=480, height=480)
 vb.plot.map(brandenburg, eintr.ve, zcol="Briefprozent",
@@ -67,4 +74,23 @@ vb.plot.map(brandenburg, eintr.all, "zuwachsprozent.zw",
             classes = c(seq(30, 120, by=15), 200, 500),
             main = "proz. Zuwachs, bez. auf Zwischenergebnis",
             palette.name = "Greens")
+dev.off()
+
+png("vergleich-nachtflug-proz-1.png")
+vb.plot.map(brandenburg, eintr.ve, "Prozent",
+            classes = c(0, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20),
+            main = "proz. Ergebnis Hochschulen erhalten")
+dev.off()
+
+png("vergleich-nachtflug-proz-2.png")
+vb.plot.map(brandenburg, nachtflug, "Prozent",
+            classes = c(0, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20),
+            main = "proz. Ergebnis Nachtflugverbot")
+dev.off()
+
+png("vergleich-nachtflug-prozpkt.png")
+vb.plot.map(brandenburg, eintr.all, "d.nachtflug.prozpkt",
+            classes = c(0, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20),
+            main = "Prozenpunkte Unterschied Hochschulen â€“ Nachtflug",
+            palette.name = "RdYlGn")
 dev.off()
